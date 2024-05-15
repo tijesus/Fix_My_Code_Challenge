b540a6a670fa9cadea12f6cf0b085a992ab7d44d
tijesus <emmanueltijesu@gmail.com>
2024-05-15 23:45:11 +0100

    square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
 
-class square():
-    """Cleass representing squareSquare """    
-    
+class Square:
+    """
+    A class representing a square shape.
+    """
 
     def __init__(self, width=0, height=0):
-        """Initializes a square with the given width and height."""
-
+        """
+        Initializes a square with the given width and height.
+        """
         self.width = width
         self.height = height
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """
+        Calculates the area of the square.
+        """
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """
+        Calculates the perimeter of the square.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
