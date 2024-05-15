09ceaf390bcdecd3d91318fe638e003e2aa08b21
tijesus <emmanueltijesu@gmail.com>
2024-05-15 23:57:38 +0100

    square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,33 +4,34 @@
 
 class Square:
     """
-    A class representing a square shape.
+       A class representing a square shape.
     """
 
     def __init__(self, width=0, height=0):
         """
-        Initializes a square with the given width and height.
+           Initializes a square with the given width and height.
         """
         self.width = width
         self.height = height
 
     def area_of_my_square(self):
         """
-        Calculates the area of the square.
+           Calculates the area of the square.
         """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
         """
-        Calculates the perimeter of the square.
+           Calculates the perimeter of the square.
         """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """
-        Returns a string representation of the square.
+           Returns a string representation of the square.
         """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
