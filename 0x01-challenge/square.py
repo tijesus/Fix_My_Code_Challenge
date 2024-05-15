c79b8857169c7372a8950c2dbfd1358607c6e2ff
tijesus <emmanueltijesu@gmail.com>
2024-05-15 23:40:54 +0100

    square fix

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class square():
+    """Cleass representing squareSquare """    
+    
+
+    def __init__(self, width=0, height=0):
+        """Initializes a square with the given width and height."""
+
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
